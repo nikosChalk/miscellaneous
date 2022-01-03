f8a894d48016491dc53f44a776309cbbfe39c89e
nikosChalk <chalkiadakisn@gmail.com>
2022-01-03 13:18:18 +0200

    update

--- gdb/.gdbinit
+++ gdb/.gdbinit
@@ -2,20 +2,6 @@
 # source /home/nikos/bin/binaries/pwndbg/gdbinit.py
 # source /home/nikos/bin/binaries/.gdbinit-gef.py
 
-define init-peda
-source ~/bin/binaries/peda/peda.py
-end
-document init-peda
-Initializes the PEDA (Python Exploit Development Assistant for GDB) framework
-end
-
-define init-pwndbg
-source ~/bin/binaries/pwndbg/gdbinit.py
-end
-document init-pwndbg
-Initializes PwnDBG
-end
-
 define init-gef
 source ~/bin/binaries/.gdbinit-gef.py
 end
@@ -26,4 +12,5 @@ end
 set history save
 set disassembly-flavor intel
 set follow-fork-mode child
+init-gef
 
--- vm-fresh-start.sh
+++ vm-fresh-start.sh
@@ -7,33 +7,14 @@
 # Stop on failure
 set -e
 
-# Fix dual boot time issue: https://askubuntu.com/questions/169376/clock-time-is-off-on-dual-boot
-sudo timedatectl set-local-rtc 1
-
 sudo apt-get update
 sudo apt-get install -y vim git gcc g++ cmake make gedit ssh gdb gdb-multiarch tmux xclip python3 curl wget glibc-doc manpages-posix manpages-posix-dev htop chromium-browser filezilla net-tools perl libc6-dbg ctags yara hydra hydra-gtk tree
 sudo apt install imagemagick nautilus-image-converter
 sudo apt-get install -y binutils file wget rpm2cpio cpio zstd build-essential
 sudo apt-get install -y ruby rubygems build-essential
-sudo apt-get install -y gnome-tweaks gnome-tweak-tool cpu-checker
 sudo apt-get install -y php php-cgi php-cli php-common php-mysql mysql-server
 sudo apt-get install -y binwalk nmap valgrind
-sudo apt-get install -y qemu virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
 
-#sticky notes
-# http://www.webupd8.org/2012/11/pin-notes-on-your-desktop-with.html
-# https://askubuntu.com/questions/245019/what-alternatives-for-sticky-notes-are-available
-sudo add-apt-repository ppa:umang/indicator-stickynotes
-sudo apt-get update
-sudo apt-get install indicator-stickynotes
-
-#kvm
-#FIXME: Ensure that `kvm-ok` outputs: "INFO: /dev/kvm exists\nKVM acceleration can be used"
-sudo apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
-sudo adduser `id -un` libvirt
-sudo adduser `id -un` kvm
-virsh list --all #FIXME: Verify the output of this command which indicates if everything was successful
-sudo apt-get install -y virt-manager
 
 # wireshark
 sudo apt-get install -y wireshark # Select that non-superusers should be able to capture packets
@@ -49,7 +30,12 @@ sudo docker run hello-world	# Verify that docker is correctly installed
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-# miscellaneous
+
+# Ubuntu specific
+sudo apt-get install -y gnome-tweaks gnome-tweak-tool cpu-checker
+
+
+# Custom binaries
 cd ~
 
 mkdir -p bin
@@ -60,25 +46,7 @@ cp miscellaneous/gdb/.gdbinit .
 cp -r miscellaneous/bin/* bin/
 ln -s $HOME/bin/binaries/remove-blank-pages.sh $HOME/bin/pdf-rm-blanks
 
-sudo snap install --classic slack
-sudo snap install --classic skype
-sudo snap install discord
-sudo snap install spotify
 sudo snap install --classic code
-sudo snap install clion --classic
-sudo snap install phpstorm --classic
-sudo snap install intellij-idea-ultimate --classic
-sudo snap install pycharm-professional --classic
-sudo snap install webstorm --classic
-
-# Configure git to store username and password
-git config --global credential.helper store
-
-#numpad that works like windows, i.e. shift+home to select whole line, etc.
-# sudoedit /etc/default/keyboard
-# Change `XKBOPTIONS=""`
-# to `XKBOPTIONS="numpad:microsoft"`
-# Then run `sudo dpkg-reconfigure keyboard-configuration` and reboot
 
 echo '' >> .bashrc
 echo '##### Automatic nikos installer #####' >> .bashrc
@@ -94,13 +62,6 @@ echo '' >> .bashrc
 echo 'export VISUAL=vim' >> .bashrc
 echo 'export EDITOR="$VISUAL"' >> .bashrc
 
-#go
-# echo '' >> .bashrc
-# echo '#go language' >> .bashrc
-# echo 'export GOROOT="/home/$USER/bin/binaries/go"' >> .bashrc
-# echo 'export GOPATH="/home/$USER/bin/binaries/gows"' >> .bashrc
-# echo 'export PATH="$PATH:$GOROOT/bin"' >> .bashrc
-# echo 'export PATH="$PATH:$GOPATH/bin"' >> .bashrc
 
 #pyenv
 sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
@@ -124,6 +85,7 @@ pyenv install 3.8.12
 pyenv global  3.8.12
 pip install --upgrade pip
 pip install ipython pylint rope matplotlib
+
 
 #pwntools
 sudo apt-get -y install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
@@ -154,22 +116,6 @@ sudo gem install one_gadget
 pip install capstone unicorn keystone-engine ropper
 wget -q -O "$HOME/bin/binaries/.gdbinit-gef.py" https://github.com/hugsy/gef/raw/master/gef.py
 
-# hyper & hyperpwn
-cd ~/Downloads
-wget https://releases.hyper.is/download/deb -O hyper.deb
-sudo apt-get -y install gconf-service gconf-service-backend gconf2 gconf2-common libappindicator1 libdbusmenu-gtk4 libgconf-2-4
-sudo dpkg -i hyper.deb
-hyper i hyperinator
-hyper i hyperpwn
-cd -
-
-#libc-database
-git clone https://github.com/niklasb/libc-database.git ~/bin/binaries/libc-database
-sudo apt-get install -y binutils file wget rpm2cpio cpio zstd jq
-cd ~/bin/binaries/libc-database
-./get all
-cd -
-
 #sqlmap
 git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git ~/bin/binaries/sqlmap
 
@@ -184,34 +130,15 @@ sudo apt-get install -y default-jre default-jdk
 
 
 ## VS Code Settings
-#TL;DR: 
 #0.) Open Visual Studio Code and install the extension Settings Sync
 #1.) Shift + Alt + D
 #2.) Paste Gist ID: 48b2d2509bb867a40e3c080a8983823b
-#3.) Press Enter
-#4.) Ctrl + P ---> ">sync" ---> "Advanced" ---> toggle auto-download and auto-upload
 
 #Burp
-sudo apt-get install -y libcanberra-gtk-module
-# Download openjdk14+36
-cd ~/Downloads
-wget https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_linux-x64_bin.tar.gz
-tar -xzf openjdk-14_linux-x64_bin.tar.gz
-sudo mv jdk-14 /usr/lib/jvm/java-14-openjdk-amd64
-cd /usr/lib/jvm
-sudo chown -R root:root java-14-openjdk-amd64
-sudo ln -s java-14-openjdk-amd64 java-1.14.0-openjdk-amd64
-ln -s /usr/lib/jvm/java-1.14.0-openjdk-amd64/bin/java $HOME/bin/java14
-cd ~
-java14 -version # verify
+## See fresh-start.sh
 
-# Install Burp now
-ln -s $HOME/bin/binaries/burpsuite-pro-v2021.8.4/run.sh $HOME/bin/burp
-# Export Certificate in DER format: ~/.BurpSuite/burp-ca.der
 
-# FireFox fane-pentest profile
-# Addon: FoxyProxy
-# Addon: Cookie Quick Manager
-# Import CA certificate: ~/.BurpSuite/burp-ca.der
+
+
 
 
